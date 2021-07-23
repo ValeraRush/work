0d1426331ce5cc7e8efd77275b400ac956f0e1f3
ValeraRush <rushexceedteam@gmail.com>
2021-07-23 11:04:17 +0300

    my commit message

---
+++ task-2.js
@@ -0,0 +1,12 @@
+//В функцию передаем массив и значение. По результату выполнения вывести все элементы массива, которые равны второму параметру.
+let array = [44,3,3321,34,4,5,6,7,5,4,776];
+testFunction=(arr,val)=>{
+    let newArray=[];
+    array.forEach((value,index)=>{
+        if (value>val){
+            newArray.push(value)
+        }
+    });
+    return newArray;
+}
+console.log(testFunction(array,7));
